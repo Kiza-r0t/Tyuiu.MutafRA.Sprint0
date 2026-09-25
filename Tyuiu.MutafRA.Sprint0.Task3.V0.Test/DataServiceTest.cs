8eaa03e6cb48729cdcee5ea67f96b95728bf0550
Роман Мутаф <Tiuiu.MutafRA@outlook.com>
2026-09-25 20:37:57 +0500

    Task3

---
+++ Tyuiu.MutafRA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MutafRA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.MutafRA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}

38df266c035f06064b558bfd5c5d9487350b7c0f
Роман Мутаф <Tiuiu.MutafRA@outlook.com>
2026-09-25 17:00:27 +0500

    Task2

---
+++ Tyuiu.MutafRA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.MutafRA.Sprint0.Task2.V0.Lib;
+
+
+namespace Tyuiu.MutafRA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void checkGetMessageValid()
+        {
+            var name = "Игорь";
+            var res = DataService.GetMessage(name);
+        }
+    }
+}

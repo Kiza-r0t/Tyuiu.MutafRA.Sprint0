65dc2cdc2fd58eb121f22d1dccab4d411a4be42e
Роман Мутаф <Tiuiu.MutafRA@outlook.com>
2026-09-25 16:25:10 +0500

    Task2

---
+++ Tyuiu.MuafRA.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MuafRA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int sum(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
